5d2cd3c76917082fe09fd174e04be93b4d605981
soheyHashimoto <heyso1209@gmail.com>
2022-08-16 02:52:54 +0900

    [ADD]何面目かを表示する

--- components/RadioButtons.tsx
+++ components/RadioButtons.tsx
@@ -6,6 +6,12 @@ interface RadioButtonsProps {
 export const RadioButtons = ({ index }: RadioButtonsProps) => {
   return (
     <div className="mx-3">
+      <div className="flex flex-col items-center">
+        <h3 className="text-center mb-2">
+          {index + 1}面目
+        </h3>
+      </div>
+
       <div className="flex flex-col">
         <div className="flex flex-col">
           <label className="flex items-center">

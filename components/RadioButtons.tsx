3a745a8b58ba39129a84d9e590fc03eb28f43dc4
soheyHashimoto <heyso1209@gmail.com>
2022-08-16 02:42:04 +0900

    [ADD]RadioButtons

---
+++ components/RadioButtons.tsx
@@ -0,0 +1,51 @@
+export const RadioButtons = () => {
+  return (
+    <div className="mx-3">
+      <div className="flex flex-col">
+        <div className="flex flex-col">
+          <label className="flex items-center">
+            {[...Array(4)].map((_, i) => (
+              <input
+                type="radio"
+                name={`radio-1-${i}`}
+                className="form-radio mx-2 mb-2"
+              />
+            ))}
+          </label>
+
+          <label className="flex items-center">
+            {[...Array(4)].map((_, i) => (
+              <input
+                type="radio"
+                name={`radio-2-${i}`}
+                className="form-radio mx-2 mb-2"
+              />
+            ))}
+          </label>
+
+          <label className="flex items-center">
+            {[...Array(4)].map((_, i) => (
+              <input
+                type="radio"
+                name={`radio-3-${i}`}
+                className="form-radio mx-2 mb-2"
+              />
+            ))}
+          </label>
+
+          <label className="flex items-center">
+            {[...Array(4)].map((_, i) => (
+              <input
+                type="radio"
+                name={`radio-4-${i}`}
+                className="form-radio mx-2"
+              />
+            ))}
+          </label>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import { RadioButtons } from "~/components/RadioButtons";
 
 const Home: NextPage = () => {
   return (
@@ -12,37 +13,16 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className='font-bold text-2xl text-center mb-8'>
+        <h1 className="font-bold text-2xl text-center mb-8">
           光らせたい場所を選択しよう！
         </h1>
 
         <div className={styles.grid}>
-          <div className={styles.card}>
-            sample
-          </div>
+          <RadioButtons />
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <RadioButtons />
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2 className="font-bold">Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <RadioButtons />
         </div>
       </main>
 
